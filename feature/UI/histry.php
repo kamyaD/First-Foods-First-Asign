575f850e394e4c9a230a9b49a77b09d00d06ecc7
kamyaD <33648833+kamyaD@users.noreply.github.com>
2018-08-29 22:45:47 +0300

    Created and linked the javascript file to the html pages. created the first function to confirm submission of order.

---
+++ feature/UI/histry.php
@@ -0,0 +1,9 @@
+<html>
+    <body>
+    <?php
+    echo $_POST["order"]
+    ?>
+
+    </body>
+</html>    
+    
--- feature/UI/script/script.js
+++ feature/UI/script/script.js
@@ -1,6 +1,14 @@
-
-
-function submiFunction() {
-    alert("The form was submitted");
+function histry(){
+    alert("Thank you for ordering at Fast Foods Fast: \n Your order is  "+document.order_form.order.value+" \n \n You will be served shortly !");
 }
-    
+
+
+function retun_order(){
+    var form = document.getElementsByTagName("order_form");
+    form_onsubmit=function(event){
+        event.preventDefault();
+        console.log(form.order);
+    }
+}
+
+retun_order();
